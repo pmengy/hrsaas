cff7384da2360b0ab35e5a2945ca4cbb5d9c5bb8
pmengy <936841379@qq.com>
2022-08-02 12:17:18 +0800

    完成主页路由权限

---
+++ src/permission.js
@@ -0,0 +1,22 @@
+import router from '@/router'
+import store from '@/store'
+// 全局前置路由守卫
+// to: 要去哪里
+// from: 来自哪里
+// next:
+const whiteList = ['/login', '/404']
+router.beforeEach((to, from, next) => {
+  const token = store.state.user.token
+  // if (token) {
+  //   to.path === '/login' ? next('/') : next()
+  // } else {
+  //   whiteList.includes(to.path) ? next() : next('/login')
+  // }
+  token
+    ? to.path === '/login'
+      ? next('/')
+      : next()
+    : whiteList.includes(to.path)
+    ? next()
+    : next('/login')
+})
